b390c9f3412a60ea14ad84085e398dbfbd8730d6
AsmaraHassan <asmarahassan939@gmail.com>
2024-09-29 17:51:49 -0700

    structure of age calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculateAge() {
+    //  birthdate yeh lyga
+    var birthDate = new Date(document.getElementById('birthdate').value);
+    var New = new Date();
+// ab age ko calculate karna hai Given date - Date of birth.
+  var age = today.getFullYear() - birthDate.getFullYear();
+var monthDifference = today.getMonth() - birthDate.getMonth();
+
+}
